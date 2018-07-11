f527bb2c8ffb8764bf725dc666a86d7d93546dc6
aguilart <calgit@gmail.com>
2018-07-11 08:57:23 +0100

    First steps

---
+++ DataPreparation.R
@@ -0,0 +1,26 @@
+##
+##
+#From our meeting on the 6th of June, we decided to create various diagrams
+#based on: 
+
+#1- Scheiner and Willig 2007
+#2- Biodiversity as are response variable or driver
+#3- General hypothesis
+#4- Vellend 2006
+#5- Classical fields
+#6- Taxonomic groups
+#7-Sutherland 100 questions in ecology
+#8-People
+
+#The most updated set of the data is available at: 
+
+#https://docs.google.com/spreadsheets/d/1gSACnoSIBW2Yeaa77o9U_5G2qWclRVb62VosrX4PQ5A/edit#gid=941463247
+
+#Options for visualization are:
+
+#https://www.data-imaginist.com/2017/introducing-tidygraph/
+
+#or: 
+
+#https://cran.r-project.org/web/packages/data.tree/vignettes/data.tree.html
+
